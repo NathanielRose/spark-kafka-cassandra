13ce1bf98315c33ead288e24d6663ec7bd8f26de
Nate <NathanielRose@users.noreply.github.com>
2018-02-20 10:53:19 -0800

    Testing DataBricks file to push to github

--- notebooks/Databricks-Kafka-Cassandra.scala
+++ notebooks/Databricks-Kafka-Cassandra.scala
@@ -3,4 +3,11 @@ dbutils.fs.mount(
   source = "wasbs://rakutentest@databricksblock.blob.core.windows.net",
   mountPoint = "/mnt/rakutentest3",
   extraConfigs = Map("fs.azure.account.key.databricksblock.blob.core.windows.net" -> "WPw7WuBSfajXVzlDz0SN3o8Cd4ZqXTH8cwnhsjzG2PimnEoJl78JLH2Bx5Jd7Jc3Mm38ATth7z9YUsuc1MAtw=="))
+
+
  
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC 1+1
